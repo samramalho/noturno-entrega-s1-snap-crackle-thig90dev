436cfe1c7eaffb3cd788ec483cc503136ec0a009
Thiago <thig90.dev@gmail.com>
2021-10-21 22:48:23 -0300

    Corrigindo a atividade

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,30 +1,28 @@
 // Seu código vai aqui
 
 function snapCrackle(maxValue){
-    let message = [];
+    let message = '';
  for(let contador = 1; contador <= maxValue; contador++){
     //  Se o número for ímpar, no lugar dele, concatenar "Snap" no final da string.
     if(contador % 2 == 1 && contador % 5 !== 0) {
-        let message1 =  'Snap, ';
-        message.push(message1);
+         message = message +  ' Snap, ';
+        
     }
     // Se o número for tanto ímpar quanto múltiplo de 5, no lugar dele, 
     // concatenar "SnapCrackle" no final da string.
     else if(contador %  2 == 1 && contador % 5 == 0){
-        let message3 = 'SnapCrackle, ';
-        message.push(message3);
+        message = message + ' SnapCrackle, ';
+        
     }
     // Se o número for múltiplo de 5, no lugar dele, concatenar "Crackle" no final da string.
     else if(contador % 5 == 0) {
-        let message2 = 'Crackle, ';
-        message.push(message2);
-    }
-    
-    
-    // Se número não for nem ímpar e nem múltiplo de 5,
-    //  concatenar o próprio número no final da string.
+        message = message + ' Crackle, ';
+        
+    }           
+    /* Se número não for nem ímpar e nem múltiplo de 5,
+      concatenar o próprio número no final da string.*/
     else if(contador %  2 !== 1 && contador % 5 !== 0) {
-            message.push(contador);
+            message = message + contador + ', ';
     }
  } console.log(message);
 } snapCrackle(12);
